d2fe71b1b24b0fa417e08a9122ad2e3d4ed729d3
Aaron Burdick <burdick.aaron@gmail.com>
2015-10-05 21:20:07 +0900

    Initial commit of new feature to also collect GPS coords

--- servcorp_scraper.js
+++ servcorp_scraper.js
@@ -8,13 +8,12 @@
 		jbuilder = require('jbuilder');
 
 	var	links = [],
-		country = [];
-
+		buildingLinks = [];
 
 
 	// Print to console in human readable format
 	function printInformation(buildingNames, address, index){
-		var currentCountry = country[index].replace(/\s/g, '');
+		var currentCountry = countryNames[index].replace(/\s/g, '');
 		console.log('****************\n' + (index+1) + '. ' + currentCountry + '\n****************');
 
 		buildingNames.forEach(function(name, i){
@@ -26,28 +25,28 @@
 	}
 
 	// Print to console in JSON
-	function printJson(buildingNames, address, index){
-		var currentCountry = country[index].replace(/\s/g, '');
+	function printJson(countryName, buildLink, buildingNames, address, index){
 
-		var output = jbuilder.create( function(json){
-			json.set(currentCountry, function(json){
+		var output = jbuilder.create(function(json){
+			json.set(countryName, function(json){
 				buildingNames.forEach(function(buildingName, index){
 					json.child( function(json){
 						json.set('name', buildingName );
 						json.set('address', address[index]);
+						json.set('link', buildLink );
 					});
 				});
 			});
-		});			
+		}); 
 
 		console.log(output.target());
 	}
 
 	// this function scrapes the servcorp website for names and link information
-	function getWorldwideNamesLinks(callback){
+	function getWorldwideNamesLinks(url, callback){
 
 		// retrieve the body HTML document
-		request('http://www.servcorp.com.au/en/worldwide-locations', function(error, response, body){
+		request(url, function(error, response, body){
 		
 			// if everything is ok then proceed
 			if(!error && response.statusCode == 200){
@@ -58,12 +57,6 @@
 				$('ul.locations li a').each( function(i, a){
 					var link = 'http://www.servcorp.com.au' + $(this).attr('href');
 					links.push(link);
-				});
-
-				// load the names into an array
-				$('ul.locations li a span').each( function(i, h3){
-					var floorName = $(this).text();
-					country.push(floorName);
 				});
 
 				// print the number of locations
@@ -79,39 +72,82 @@
 		
 	}
 
-	function getBuildingNamesAddresses(url, index, callback){
-		
-		var addresses = [];
-		var buildingNames = [];
+	function getInfo (url, buildingName, type, selector, callback) {
+	  
+	  var res = '';
+	  var res2 = '';
 
 		request(url, function(error, response, body){
 			if(!error && response.statusCode == 200){
 				var $ = cheerio.load(body);
 
-				$('p.building-address', 'div.building-details').each(function(i, span){
-					addresses[i] = $(this).html();
-					addresses[i] = addresses[i].replace (/\<br\>/g, ' ' );
-				});
+				if(type ==='text' && body){
+	   			$(selector[0], selector[1]).each(function( index, element ){
+		   			res = $(element).text().replace(/\s{2,}/g, '');
+				  });
+				} else if (type === 'html') {
+  				$(selector[0][0], selector[0][1]).each( function( index, element){
+  					res = $(element).html().replace(/\s{2,}/g, '');
+  				});
+  				$(selector[1][0], selector[1][1]).each( function( index, element){
+  					res2 = $(element).html().match(/\d{1,3}.\d{1,}/g);
+  				});
+  				
+				} else {
+				  throw error;
+				}
 
+			}
+			callback && callback(res, res2, buildingName);
+		});
+	}
+
+	function getBuildingNamesAddresses(url, index, callback){
+		
+		var countryName = "";
+		var addresses = [];
+		var buildingName = '';
+		var buildingLink = '';
+
+		request(url, function(error, response, body){
+			if(!error && response.statusCode == 200){
+				var $ = cheerio.load(body);
+
+				countryName = $('.module.page-title h1', '.last').text().replace(/[\s]{2,}/g, '');
+
+				// $('p.building-address', 'div.building-details').each(function(i, span){
+				// 	addresses[i] = $(this).html();
+				// 	addresses[i] = addresses[i].replace (/\<br\>/g, ' ' );
+				// });
 				$('p.building-name strong', 'div.building-details').each(function(i, strong){
-					buildingNames[i] = $(this).text();
+					buildingName = $(this).text();
 				});	
+
+				$('a.button-small-round-white:first-child', '.building-call-to-action').each( function(i, a){
+					buildingLink = $(this).attr('href');
+					callback && callback( buildingLink, buildingName );
+				});
 
 			} else {
 				console.log("There was an error retrieving the information.");
 			}
 
-			printJson(buildingNames, addresses, index);
-
+			// printJson(countryName, buildingLink, buildingNames, addresses, index);
+				
 		});
-		
-		callback && callback();	
 	}
 
 
-	getWorldwideNamesLinks( function(){
+	getWorldwideNamesLinks('http://www.servcorp.com.au/en/worldwide-locations', function(){
 		links.forEach( function(link, i){
-			getBuildingNamesAddresses(link, i);
+//			getBuildingNamesAddresses(link, i);
+			getBuildingNamesAddresses(link, i, function( buildingLink, buildingName ){
+				getInfo(buildingLink, buildingName, 'html', [['p','.location-info address'],['script','.module.building-location-map .column-container']], function(res, res2, buildingName){
+					console.log(res);
+					console.log(res2);
+					console.log(buildingName);
+				});
+			});
 		});
 	});
 
